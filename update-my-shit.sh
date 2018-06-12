61968d0aebdd13c5772c8e9c3cca9bc3adc48321
Gauthier Roebroeck <gauthier.roebroeck@gmail.com>
2018-06-12 16:13:57 -0700

    initial commit

---
+++ update-my-shit.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+LOGDIR="${HOME}/Library/Logs/ums"
+mkdir -p $LOGDIR
+
+run(){
+    local command=$1
+    local logfile="${LOGDIR}/${2}"
+    local timestamp=$(date +"[%Y-%m-%d %H:%M:%S]")
+
+    echo "${timestamp} ${command}" | tee -a $logfile
+    eval $command | tee -a $logfile
+}
+
+run "brew update" "brew-update.log"
+run "brew upgrade" "brew-upgrade.log"
+run "brew cleanup" "brew-cleanup.log"
+run "npm -g update" "npm-update.log"
+run "pip list --outdated" "pip-update.log"
+run "pip list --outdated --format=freeze | grep -v '^\-e' | cut -d = -f 1  | xargs -n1 pip install -U" "pip-update.log"
+run "fish -c 'omf update'" "omf-update.log"
